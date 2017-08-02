c5d95582681af2be67a50bc046eecec4c33c7d58
ksugawara61 <katsuya@kokohadoko.net>
2017-08-02 14:26:41 +0900

    カウンドアプリを作成

--- CountApp3/ViewController.swift
+++ CountApp3/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    var num: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func plus() {
+        num = num + 1
+        label.text = String(num)
+    }
 
-
+    @IBAction func minus() {
+        num = num - 1
+        label.text = String(num)
+    }
+    
+    @IBAction func clear() {
+        num = 0
+        label.text = String(num)
+    }
 }
 
