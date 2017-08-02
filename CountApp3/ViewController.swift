62f2f6eb1929a1915333a4b8fd54c808bd1ee0a1
ksugawara61 <katsuya@kokohadoko.net>
2017-08-02 13:26:58 +0900

    Initial Commit

---
+++ CountApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountApp3
+//
+//  Created by 菅原勝也 on 2017/08/02.
+//  Copyright © 2017年 instil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
